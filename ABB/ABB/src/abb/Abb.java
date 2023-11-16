4dfa9ef86ea412fca59fc0d5307e9b3150492f8b
Alejandro <diegoelizalde475@gmail.com>
2023-11-15 19:32:30 -0600

    Add a new functions to the class Abb.java and Main

--- ABB/ABB/src/abb/Abb.java
+++ ABB/ABB/src/abb/Abb.java
@@ -1,20 +1,20 @@
 package abb;
-
+//Declaracion de la clase Abb
 class Abb {
     Node root;
 
     public Abb() {
         root = null;
     }
-
+    //Check if the tree is empty
     public boolean isEmpty() {
         return root == null;
     }
-
+    //Insert data in the tree
     public void insert(int data) {
         root = insertRec(root, data);
     }
-
+    //Insert data in the tree
     private Node insertRec(Node root, int data) {
         if (root == null) {
             root = new Node(data);
@@ -27,7 +27,7 @@ class Abb {
         }
         return root;
     }
-
+    //Order the tree
     public void inOrder() {
         inOrderRec(root);
     }
@@ -63,6 +63,81 @@ class Abb {
             System.out.println(root.data);
         }
     }
+    //Search a node in the tree
+    public void searchRec(Node root, int data) {
+        if (root != null) {
+            if (root.data == data) {
+                System.out.println("El número " + data + " se encuentra en el árbol binario de búsqueda");
+            } else if (data < root.data) {
+                searchRec(root.left, data);
+            } else if (data > root.data) {
+                searchRec(root.right, data);
+            }
+        } else {
+            System.out.println("El número " + data + " no se encuentra en el árbol binario de búsqueda");
+        }
+    }
+    //Determines the number of leaves in the tree
+
+    public int getLeafCountRec(Node root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        } else {
+            return getLeafCountRec(root.left) + getLeafCountRec(root.right);
+        }
+    }
+    //Calculate the height of the tree
+    public int height(Node root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            if (leftHeight > rightHeight) {
+                return (leftHeight + 1);
+            } else {
+                return (rightHeight + 1);
+            }
+        }
+    }
+    //Delete a node in the tree
+    public void delete(int data) {
+        
+        root = deleteRec(root, data);
+    }
+    private deleteRec(Node root, int data) {
+        if (root == null) {
+            return root;
+        }
+        if (data < root.data) {
+            root.left = deleteRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = deleteRec(root.right, data);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            root.data = minValue(root.right);
+            root.right = deleteRec(root.right, root.data);
+        }
+        return root;
+    }
+    //Find the minimum value in the tree
+    public int minValue(Node root) {
+        int minv = root.data;
+        while (root.left != null) {
+            minv = root.left.data;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    
+
 
     public void exit() {
         System.exit(0);
--- ABB/ABB/src/abb/Main.java
+++ ABB/ABB/src/abb/Main.java
@@ -7,15 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Abb tree = new Abb();
 
-        int menu;
-        System.out.println("Ingrese el número de la opción que desea realizar: ");
-        System.out.println("1. Insertar un nodo en el árbol binario de búsqueda");
-        System.out.println("2. Recorrer el árbol binario de búsqueda en orden");
-        System.out.println("3. Recorrer el árbol binario de búsqueda en preorden");
-        System.out.println("4. Recorrer el árbol binario de búsqueda en postorden");
-        System.out.println("5. Salir");
-        menu = scanner.nextInt();
-
+        int menu = 0;
         do{
             switch(menu){
                 case 1:
@@ -38,6 +30,22 @@ public class Main {
                 case 5:
                     System.out.println("Saliendo del programa...");
                     break;
+                case 6:
+                    System.out.println("Ingrese el número que desea buscar en el árbol binario de búsqueda: ");
+                    int dataSearch = scanner.nextInt();
+                    tree.searchRec(tree.root, dataSearch);
+                    break;
+                case 7:
+                    System.out.println("El número de hojas del árbol binario de búsqueda es: " + tree.getLeafCountRec(tree.root));
+                    break;
+                case 8:
+                    System.out.println("La altura del árbol binario de búsqueda es: " + tree.height(tree.root));
+                    break;
+                case 9:
+                    System.out.println("Eliminar un nodo del árbol binario de búsqueda: ");
+                    int dataDelete = scanner.nextInt();
+                    tree.delete(dataDelete);
+                    break;
                 default:
                     System.out.println("Opción no válida");
                     break;
@@ -48,6 +56,10 @@ public class Main {
             System.out.println("3. Recorrer el árbol binario de búsqueda en preorden");
             System.out.println("4. Recorrer el árbol binario de búsqueda en postorden");
             System.out.println("5. Salir");
+            System.out.println("6. Ingresar un número para buscarlo en el árbol binario de búsqueda");
+            System.out.println("7. Determinar el número de hojas del árbol binario de búsqueda");
+            System.out.println("8. Determinar la altura del árbol binario de búsqueda");
+            
             menu = scanner.nextInt();
         }while(menu != 5);
 
